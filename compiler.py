413b384c7fc847985143a4bbe4148cd36be9eade
alphabert23 <albertpardillo42@gmail.com>
2021-02-17 22:26:23 +0800

    added hello.tiny.txt

--- compiler.py
+++ compiler.py
@@ -1,12 +1,26 @@
 from lexer import *
+from parse import *
+import sys
 
 def main():
-    input = "INT kungpao = 123 \0"
-    lexer = Lexer(input)
+    print("Teeny Tiny Compiler")
 
-    token = lexer.getToken()
-    while token.kind != TokenType.EOF:
-        print(token.kind)
-        token = lexer.getToken()
+    if len(sys.argv) != 2:
+        sys.exit("Error: Compiler needs source file as argument.")
+    with open(sys.argv[1], 'r') as inputFile:
+        input = inputFile.read()
+
+
+    #input = "LET kungpao = 123 \0"
+    lexer = Lexer(input)
+    parser = Parser(lexer)
+
+    parser.program()
+    print("Parsing completed.")
+
+    #token = lexer.getToken()
+    #while token.kind != TokenType.EOF:
+    #    print(token.kind)  
+    #    token = lexer.getToken()
 
 main()
